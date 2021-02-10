0cc35122af764c456be6a197c00607e54a98c4a0
DevZezo <74310751+DevZezo@users.noreply.github.com>
2021-02-10 12:47:14 +0300

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+    string name = "zezo";
+    Console.WriteLine(name);
+    int age = 15;
+    Console.WriteLine(age);
+    bool developer = true;
+    Console.WriteLine(developer);
+  }
+}
